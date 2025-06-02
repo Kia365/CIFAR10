414f69b1ed9b98a56003ba134ec23118003cda5e
KIA <kianushpourazarinbox@gmail.com>
2025-06-02 06:18:03 +0330

    changed the file name

--- app.py
+++ app.py
@@ -8,26 +8,23 @@ import torch.nn as nn
 classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 
            'dog', 'frog', 'horse', 'ship', 'truck']
 
-# Load model
 model = models.resnet18()
-model.fc = nn.Linear(model.fc.in_features, 10)  # for CIFAR-10
-model.load_state_dict(torch.load("resnet_cifar10.pth", map_location="cpu"))
+model.fc = nn.Linear(model.fc.in_features, 10)
+model.load_state_dict(torch.load("fine_tuned_model.pth", map_location="cpu"))
 model.eval()
 
-# Image transform
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 ])
 
-# Streamlit UI
-st.title("CIFAR-10 Image Classifier")
+st.title("Image Classifier")
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     input_tensor = transform(image).unsqueeze(0)
 
